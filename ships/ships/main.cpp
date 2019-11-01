3fb01157ce2659dec102bc0430804f81757a30c3
Blohh <agnieszka.omernik@gmail.com>
2019-11-01 23:24:57 +0100

    Add functions to print field

--- ships/ships/main.cpp
+++ ships/ships/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <time.h>
 
+using namespace std;
+
 #define SIZE 10
+
 //fill field with 0 - which means empty place on map
 void fillField(char field[][SIZE])
 {
@@ -23,11 +26,7 @@ bool checkPosition(char field[][SIZE], int ship_size, int direction, int x, int 
 	}
 	return true;
 }
-/*randomly place ships on the field 
-4 ships of size 1, 
-3 ships of size 2, 
-2 ships of size 3, 
-1 ship of size 4 */ 
+//place ship of size ship_size at direction direction, starts by point x, y
 void placeShip(char field[][SIZE], int ship_size, int direction, int x, int y)
 {
 	for (int i = 0; i < ship_size; i++)
@@ -38,7 +37,11 @@ void placeShip(char field[][SIZE], int ship_size, int direction, int x, int y)
 			field[x][y + i] = (-1)*ship_size;
 	}
 }
-
+/*randomly place ships on the field
+4 ships of size 1,
+3 ships of size 2,
+2 ships of size 3,
+1 ship of size 4 */
 void placeShipsOnField(char field[][SIZE])
 {
 	for (int ship_size = 1; ship_size <= 4; ship_size++)
@@ -56,6 +59,37 @@ void placeShipsOnField(char field[][SIZE])
 		}
 	}
 }
+void printHeader()
+{
+	cout << "    ";
+	for (int i = 0; i < SIZE; i++) cout << (char)(i + 'A') << " ";
+	cout << endl << "   ";
+	for (int i = 0; i < SIZE; i++) cout << " -";
+	cout << endl;
+}
+void printFooter()
+{
+	cout << "   ";
+	for (int i = 0; i < SIZE; i++) cout << " -";
+	cout << endl << "    ";
+	for (int i = 0; i < SIZE; i++) cout << (char)(i + 'A') << " ";
+	cout << endl;
+}
+void printField(char field[][SIZE], bool cheat)
+{
+	printHeader();
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << i << " | ";
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (field[i][j] > 0) cout << (char)(field[i][j] + '0') << " ";
+			else if (cheat && field[i][j] < 0) cout << (char)(-1 * field[i][j] + '0') << " ";
+			else cout << "  ";
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	srand(time(NULL));

0d85b306b7086ae777e93dd72397134eade1f82e
Blohh <agnieszka.omernik@gmail.com>
2019-11-01 23:02:30 +0100

    Add declaration of game field

---
+++ ships/ships/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <time.h>
+
+#define SIZE 10
+
+int main(int argc, char* argv[])
+{
+	srand(time(NULL));
+	//Declaration od the game field
+	char field[SIZE][SIZE];
+	return 0;
+}
